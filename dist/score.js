942125358042fa7fcefe0b7e4927f17cf5214794
RyosukeSomeya <rx.78.0618@gmail.com>
2021-01-21 14:02:07 +0900

    webpack-dev-server追加

---
+++ dist/score.js
@@ -0,0 +1,18 @@
+import { Foods } from "./foods";
+// 食べ物のスコアを合計するクラス
+export class Score {
+    constructor() { }
+    get totalScore() {
+        const foods = Foods.getInstance();
+        return foods.activeElementsScore.reduce((total, score) => total + score);
+    }
+    render() {
+        document.querySelector('.score__number').textContent = String(this.totalScore);
+    }
+    static getInstance() {
+        if (!Score.instance) {
+            Score.instance = new Score();
+        }
+        return Score.instance;
+    }
+}
